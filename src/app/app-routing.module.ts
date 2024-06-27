a2b0d9923f695d4076303a5b071c81c05ecfb8d9
Amr <amrraafat17@yahoo.com>
2024-06-27 03:30:34 +0300

    init routing app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   {path:"home", pathMatch:'full', loadChildren:()=>import('./features/pages/home/home.module').then(m=>m.HomeModule)},
+
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
+  {path:'login', pathMatch:'full', loadChildren:()=>import('./features/pages/login/login.module').then(m=>m.LoginModule)},
 
+  {path:'products', pathMatch:'full', loadChildren:()=>import('./features/pages/products/products.module').then(m=>m.ProductsModule)},
 
 ];
 
