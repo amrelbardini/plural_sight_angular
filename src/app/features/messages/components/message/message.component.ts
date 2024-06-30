0cc7f3aa81a6bfd7505c7f06162189783e04f004
Amr <amrraafat17@yahoo.com>
2024-06-30 23:06:17 +0300

    secondery routes done!

--- src/app/features/messages/components/message/message.component.ts
+++ src/app/features/messages/components/message/message.component.ts
@@ -5,31 +5,25 @@ import { MessagesService } from '../../services/messages.service';
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  styleUrls: ['./message.component.scss'],
 })
 export class MessageComponent implements OnInit {
+  constructor(private router: Router, private message: MessagesService) {}
 
-  constructor(private router:Router,private message:MessagesService) { }
-
-  ngOnInit(): void {
-  }
-  closeMessage(){
-
-    this.message.isShown=false;
+  ngOnInit(): void {}
+  closeMessage() {
+    this.message.isShown = false;
 
     this.hideMessages();
-    console.log("-----------HIDE MESSAGE FROM MESSAGE COMP----------------")
-
-    console.log(this.message.isShown)
   }
 
-  hideMessages(){
-    this.router.navigate([{outlets:{
-      popup:null
-    }}])
-
+  hideMessages() {
+    this.router.navigate([
+      {
+        outlets: {
+          popup: null,
+        },
+      },
+    ]);
   }
-
-
-
 }
