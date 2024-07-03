586f927c554eef207331123b4ced4a9575d3e37d
Amr <amrraafat17@yahoo.com>
2024-07-03 08:50:45 +0300

    ...

--- src/app/features/pages/products/components/product-edit/product-edit.component.ts
+++ src/app/features/pages/products/components/product-edit/product-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import {  Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
 import { ProductsService } from '../../services/products.service';
 
