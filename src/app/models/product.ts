64be9b4ff46b03c3dcaa6c3270e24a496f6a43bc
Amr <amrraafat17@yahoo.com>
2024-08-04 13:30:48 +0300

    interface to model

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,10 +1,10 @@
-export interface Product {
-  title: string;
-  price: number;
-  description: string;
-  image: string;
-  category: string;
-  rating : {
+export class Product {
+  title: string="";
+  price: number=0;
+  description: string="";
+  image: string="";
+  category: string="";
+  rating = {
     rate: '',
     count: 0,
   };
